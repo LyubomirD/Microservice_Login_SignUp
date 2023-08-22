80f3f15b37cf9cc9348cf6e4660d82c67170b05a
Lyubo <lubodimoff@gmail.com>
2023-08-22 22:40:04 +0300

    Solution the 403 status, new error 401 status

--- src/main/java/webapplication/login_signup/MicroserviceLoginSignUpApplication.java
+++ src/main/java/webapplication/login_signup/MicroserviceLoginSignUpApplication.java
@@ -2,11 +2,12 @@ package webapplication.login_signup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-@SpringBootApplication(scanBasePackages = "webapplication.login_signup.RestApi")
+@SpringBootApplication(scanBasePackages = "webapplication.login_signup.RestApi", exclude = SecurityAutoConfiguration.class)
 @EnableWebMvc
 @ControllerAdvice
 public class MicroserviceLoginSignUpApplication {
--- src/test/java/webapplication/login_signup/MicroserviceLoginSignUpApplicationTests.java
+++ src/test/java/webapplication/login_signup/MicroserviceLoginSignUpApplicationTests.java
@@ -1,9 +1,7 @@
 package webapplication.login_signup;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MicroserviceLoginSignUpApplicationTests {
 
     @Test

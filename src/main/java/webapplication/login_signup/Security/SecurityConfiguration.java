57fa6d6cf023010288cbcf58b40954f74a8900e2
Lyubo <lubodimoff@gmail.com>
2023-08-25 17:43:38 +0300

    Removing commented out code, I am sure that I would not needed

--- src/main/java/webapplication/login_signup/Security/SecurityConfiguration.java
+++ src/main/java/webapplication/login_signup/Security/SecurityConfiguration.java
@@ -8,57 +8,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    private final CustomUserDetailsService userDetailsService;
-//
-//    public SecurityConfiguration(CustomUserDetailsService userDetailsService) {
-//        this.userDetailsService = userDetailsService;
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//    }
-
-    //Original
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/usersRegistration/**").permitAll()
-//                .and()
-//                .csrf().disable();
-//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/usersRegistration/**").permitAll() // Allow unauthenticated access
-                .anyRequest().authenticated() // Require authentication for other endpoints
+                .antMatchers("/usersRegistration/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .cors() // Enable Spring Security's CORS handling
+                .cors()
                 .and()
-                .csrf().disable(); // Disable CSRF protection
+                .csrf().disable();
     }
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("*"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        configuration.setAllowCredentials(true);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//
-//
 }
 
